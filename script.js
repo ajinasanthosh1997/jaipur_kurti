324448f05eac11285c08e916e0deee5c7cd7b509
Ajina Santhosh <Ajina Santhosh>
2023-08-13 21:26:31 +0530

    updated homepage

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+
 $(document).ready(function(){
     $("#owl-demo").owlCarousel({
         autoPlay:3000,
@@ -147,3 +148,140 @@ slides.forEach(
     slide.style.left='${index * 100}%'
   }
 )
+
+function myFunction1(){
+  var x=document.getElementById("myTopnav");
+  if(x.className === "nvbr"){
+    x.className += " responsive";
+  }else {
+    x.className = "nvbr";
+  }
+}
+const body = document.querySelector("body");
+const darkLight = document.querySelector("#darkLight");
+const sidebar = document.querySelector(".sidebar");
+const submenuItems = document.querySelectorAll(".submenu_item");
+const sidebarOpen = document.querySelector("#sidebarOpen");
+const sidebarClose = document.querySelector(".collapse_sidebar");
+const sidebarExpand = document.querySelector(".expand_sidebar");
+sidebarOpen.addEventListener("click", () => sidebar.classList.toggle("close"));
+
+sidebarClose.addEventListener("click", () => {
+  sidebar.classList.add("close", "hoverable");
+});
+sidebarExpand.addEventListener("click", () => {
+  sidebar.classList.remove("close", "hoverable");
+});
+
+sidebar.addEventListener("mouseenter", () => {
+  if (sidebar.classList.contains("hoverable")) {
+    sidebar.classList.remove("close");
+  }
+});
+sidebar.addEventListener("mouseleave", () => {
+  if (sidebar.classList.contains("hoverable")) {
+    sidebar.classList.add("close");
+  }
+});
+
+darkLight.addEventListener("click", () => {
+  body.classList.toggle("dark");
+  if (body.classList.contains("dark")) {
+    document.setI
+    darkLight.classList.replace("bx-sun", "bx-moon");
+  } else {
+    darkLight.classList.replace("bx-moon", "bx-sun");
+  }
+});
+
+submenuItems.forEach((item, index) => {
+  item.addEventListener("click", () => {
+    item.classList.toggle("show_submenu");
+    submenuItems.forEach((item2, index2) => {
+      if (index !== index2) {
+        item2.classList.remove("show_submenu");
+      }
+    });
+  });
+});
+
+
+const menuToggleBtn=document.querySelector(".menu-toggle");
+const navlist=document.querySelector(".nvbr");
+
+menuToggleBtn.addEventListener("click",function(){
+  navlist.classList.toggle("active");
+});
+
+// navbar
+
+function toggleMenu() {
+  var navbar = document.getElementById("navbar");
+  navbar.classList.toggle("show");
+  document.getElementById("slideshow-container").style.paddingTop='500px';
+}
+
+function toggleDropdown(event) {
+  var dropdown = event.target.closest(".drpdwn");
+  if (dropdown) {
+    dropdown.classList.toggle("show-dropdown");
+  }
+}
+
+// Attach click event to the document to handle dropdown toggling
+document.addEventListener('click', toggleDropdown);
+function toggleNextDivPosition() {
+  var nextDiv = document.querySelector('.ne');
+  nextDiv.classList.toggle('down');
+}
+
+// close
+$(document).ready(function(){
+  $('.owl-carousel2').owlCarousel({
+    items: 1, // Number of items to show on smaller screens
+    loop: true,
+    nav: true,
+    dots: true,
+    responsive: {
+      // Responsive settings for larger screens (optional)
+      768: {
+        items: 3, // Number of items to show on larger screens
+      }
+    }
+  });
+});
+function myfun2(){
+  var x=document.getElementById("mydiv")
+  if (x.style.display === 'none'){
+    x.style.display='block';
+  } else{
+    x.style.display='none';
+  }
+}
+
+$(document).ready(function(){
+  $(".owl-carousel").owlCarousel({
+    loop: true,
+    margin: 10,
+    nav: true, // Change to 'false' if you don't want navigation arrows
+    responsive: {
+      0: {
+        items: 1 // Show 1 item on screens of all sizes (width <= 0px)
+      }
+    }
+  });
+});
+
+
+document.addEventListener("click", function(event) {
+  var navbar = document.querySelector(".navbar");
+  var targetElement = event.target; // Element that was clicked
+
+  // Check if the clicked element is outside the navbar
+  
+     if (!navbar.contains(targetElement)) {
+    // The click was outside the navbar, so hide the navbar or perform any other action here.
+    navbar.style.display = "none"; // Example: hide the navbar
+  }
+
+});
